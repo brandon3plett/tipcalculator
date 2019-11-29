26eab9a81f5012ec08b436bc836771526d582627
brandon3plett <btriplett216757@gmail.com>
2019-11-28 22:58:25 -0500

    Newer files/syntax

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# tipcalculator
-Just a calculator for quick tipping estimates
+Tip Calculator
+
+In this exercise, you're tasked with creating a tip calculator using(but not limited to) the following:
+
+-HTML
+-CSS
+-Javascript/jQuery
+
+Key tips:
+
+-Start out with working with the front end.  Try implementing the mobile first methodology.
+
+-When you start to work on the back end, you'll be able to easily connect it to the front end using it's ids(or classes).
+
+-Really challenge yourself.  Try not to just copy and paste someone else's code to build it.  You're more than welcome though, to look at other's code to get an idea of how you want to build it, how you want it to look, etc.
+
+-A good source would be https://www.codepen.io
--- assets/js/calculate.js
+++ assets/js/calculate.js
@@ -1,24 +1,54 @@
-let subTotal = document.getElementById("subtotal-input");
-let customTip = document.getElementById("custom-tip-input");
-let calculateBtn = document.getElementById("calculate-btn");
-let calculatedTip = document.getElementById("calculated-tip");
-let totalAndTip = document.getElementById("total-and-tip");
-let dropdown = document.querySelector(".dropdown");
+//Variables
+let subTotal = document.getElementById("subtotal-input")
+let customTip = document.getElementById("custom-tip-input")
+let calculateBtn = document.getElementById("calculate-btn")
+let calculatedTip = document.getElementById("calculated-tip")
+let grandTotal = document.getElementById("grand-total")
+let dropDown = document.getElementById("percentage-dropdown")
 
-const calculatePercentage = (subtotal, percentage) => {
-    let totalWithTip = subtotal * percentage;
-    calculatedTip.textContent = parseFloat(totalWithTip).toFixed(2);
+
+//Function to calculate the sub-total amount and multipy it with the chosen percentage
+calculatePercentage = (subtotal, percentage) => {
+    //Take the product of the subtotal and the chosen percentage and store in a variable
+    let tipValue = Number(subtotal) * Number(percentage)
+    //Take the variable tipValue and write to the DOM
+    calculatedTip.textContent = tipValue.toFixed(2)
+    //Take the total of the subtotal value and the tipValue and write to the DOM
+    grandTotal.textContent = (Number(subTotal.value) + Number(tipValue)).toFixed(2)
 }
 
-calculateBtn.onclick = () => {
-    if(dropdown.value == "") {
-        return false
-    }
+//Function to calculate subtotal plus custom tip
+calculateCustomTip = (subtotal, customNum) => {
+    //Take the total of the subtotal and the custom number and store in a variable
+    let tipValue = Number(subtotal) + Number(customNum)
+    //Take the custom number and write it to the DOM
+    calculatedTip.textContent = Number(customNum).toFixed(2)
+    //Take the total of the subtotal value and the custom number and write to the DOM
+    grandTotal.textContent = (Number(subTotal.value) + Number(customNum)).toFixed(2)
+}
 
-    if (!isNaN(subTotal.value) && subTotal.value !== "") {
-        let subTotalParse = parseFloat(subTotal.value);
-        let dropdownParse = parseFloat(dropdown.value);
-        calculatePercentage(subTotalParse, dropdownParse);
-        totalAndTip.textContent = (subTotalParse + parseFloat(calculatedTip.textContent)).toFixed(2);
+//Function that determines what happens upon Calculate button click
+calculateBtn.onclick = () => {
+    //If there's a value in the sub-total input field...
+    if (Number(subTotal.value) && subTotal.value !== "") {
+        //If there's a percentage selected AND a custom tip value input...
+        if (dropDown.value && Number(customTip.value) && customTip.value !== "") {
+            //console.log("Too many enteries.")
+            return false
+            //Else if only there's a percentage selected...
+        } else if (dropDown.value) {
+            calculatePercentage(subTotal.value, dropDown.value)
+            //Else if there's only a custom tip value...
+        } else if (Number(customTip.value) && customTip.value !== "") {
+            calculateCustomTip(subTotal.value, customTip.value)
+            //Else...
+        } else {
+            //console.log("Nope.  This won't do.")
+            return false
+        }
+        //Else...
+    } else {
+        //console.log("Nope.  This won't do.")
+        return false
     }
 }
